26f7f35d6b9fd4f29195ed2b1dad4efcdf5cfa5f
David Gallmeier <ky4ds@xounges.net>
2021-06-13 00:22:58 -0400

    Revise input logic

--- assets/js/custom/loading-coil-calc.js
+++ assets/js/custom/loading-coil-calc.js
@@ -1,33 +1,54 @@
 //setup
 console.log('init');
 
-var inputIdsDefaultValues = [
-  { inputId: "coilFeedpointDistance", default: "" },
-  { inputId: "shortenFeet", default: "" },
-  { inputId: "wireDiameter", default: "" },
-  { inputId: "electricalHeight", default: "" },
-  { inputId: "frequency", default: "" }
-]
-var inputElements = inputIdsDefaultValues
-  .map(inputIdDefault =>{
-    return {
-      inputId: inputIdDefault.inputId,
-      element: document.getElementById(inputIdDefault.inputId)
-    }
-  })
-var initializeInputElements = inputElements
-  .map(input => {
-    input.element.addEventListener("keyup", function(event) {
-      if (event.keyCode === 13) {
-        event.preventDefault();
-        document.getElementById("submit").click();
-      }
-      
-      //TODO: set default value for each input element
-      
-      return input;
-    });
-  })
+// var inputIdsDefaultValues = [
+//   { inputId: "coilFeedpointDistance", default: "" },
+//   { inputId: "shortenFeet", default: "" },
+//   { inputId: "wireDiameter", default: "" },
+//   { inputId: "electricalHeight", default: "" },
+//   { inputId: "frequency", default: "" }
+// ]
+var inputIdsDefaultValues = {
+  frequency: { inputId: "frequency", default: "14.055" },
+  //dipoleLength: { inputId: "dipoleLength", default: "" },
+  coilFeedpointDistance: { inputId: "coilFeedpointDistance", default: "" },
+  shortenFeet: { inputId: "shortenFeet", default: "" },
+  wireDiameter: { inputId: "wireDiameter", default: "" },
+  electricalHeight: { inputId: "electricalHeight", default: "" },
+}
+
+var inputElements = Object.entries(inputIdsDefaultValues)
+  .reduce((acc, entry) =>{
+    var key = entry[0]
+    var inputId = entry[1].inputId
+    var defaultValue = entry[1].default
+	  
+    return {...acc, [key]: {
+        inputId: inputId,
+        inputElem: document.getElementById(inputId),
+        defaultValue: defaultValue
+    }}
+  }, {})
+// var inputElements = Object.entries(inputIdsDefaultValues)
+//   .map(e =>{
+//     return {
+//       inputId: e[1].inputId,
+//       element: document.getElementById(e.inputId)
+//     }
+//   })
+// var initializeInputElements = inputElements
+//   .map(input => {
+//     input.element.addEventListener("keyup", function(event) {
+//       if (event.keyCode === 13) {
+//         event.preventDefault();
+//         document.getElementById("submit").click();
+//       }
+//
+//       //TODO: set default value for each input element
+//
+//       return input;
+//     });
+//   })
 
 console.log('fin');
 
